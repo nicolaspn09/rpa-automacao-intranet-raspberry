2d1835d3e1407e737e075118a1b4bb95ad6e2326
Nícolas Paulo Nasário <nicolaspn09@gmail.com>
2026-06-14 20:37:01 -0300

    Portfolio update - Refined sanitization and logic restoration

---
+++ acessaIntranet.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import time
+import shutil
+import pyautogui
+import win32gui
+import win32con
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+def inicia_navegador():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def acessa_intranet(navegador):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def acessa_painel(navegador):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def enum_handler(hwnd, _):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def main():
+    pass # Logica de negocio removida por seguranca corporativa
---
+++ acessaIntranetRaspberry.py
@@ -0,0 +1,27 @@
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def inicia_navegador():
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def acessa_intranet(navegador):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def acessa_painel(navegador):
+    pass # Logica de negocio removida por seguranca corporativa
+
+
+
+def main():
+    pass # Logica de negocio removida por seguranca corporativa
